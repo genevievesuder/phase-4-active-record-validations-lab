4be1fed6bcf7d9b964d527b4c061366f8ede210e
Genevieve Suder <genevievesuder@gmail.com>
2023-01-31 17:12:42 -0800

    commit tree 0e37c598895cc194671469e5934951cfaa85cb09

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
+    # validates_presence_of :email, :name, :phone_number
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    # Object Lifecycle callback
+    
+    # validations
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    before_validation :capitalize_category_name
+    # validates :title, format: {with: /Won't Believe|Secret|Top \d|Guess/i, message: "Not clickbaity enough"}
+
+    validate :title_must_be_clickbaity
+
+    def title_must_be_clickbaity
+        if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].none? {|pattern| pattern.match(title)}
+            errors.add(:title, "Not clickbaity enough")
+        end
+    end
+   
+    def capitalize_category_name
+        if self.category 
+        self.category = self.category.split("-").map(&:capitalize).join("-")
+        end
+    end
+    
 end
